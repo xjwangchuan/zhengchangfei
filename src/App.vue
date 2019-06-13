83f914c562f0fafa10d645aee50831c35db7a76d
xjwangchuan <vuexjwangchuan@163.com>
2019-06-13 17:20:12 +0800

    添加stylus

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,17 @@ export default {
 }
 </script>
 
-<style>
-#app {
+<style lang="stylus">
+#app 
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+
+  img 
+    width 500px
+    height 500px
+
 </style>
